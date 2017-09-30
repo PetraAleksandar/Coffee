1496ff4f6df540b311dbc0b1634cf40e05122042
PetraAleksandar <aleksandar.debeljak.91@gmail.com>
2017-09-30 18:17:34 +0200

    solving sort

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var link = 'https://api.foursquare.com/v2/venues/explore?client_id=XEDDHYATVRKSYW13C5HAEETLPRYAQTAPYRWWUEBZ4WZ3PRNY &client_secret=5TSQ0XOYK4HDVPJSLWC30I5DADXHHPFG54GHU30IBGYNHUKJ &ll=44.8,20.46 &query=coffee &radius=1000 &openNow=1 &sortByDistance=1 &venuePhotos=1&oauth_token=MKKKAZSMP4C3CCJUPHGERNQFSMUNRPSLTNEAUQKIYEITBBJR&v=20170930';
+var link = 'https://api.foursquare.com/v2/venues/explore?client_id=XEDDHYATVRKSYW13C5HAEETLPRYAQTAPYRWWUEBZ4WZ3PRNY &client_secret=5TSQ0XOYK4HDVPJSLWC30I5DADXHHPFG54GHU30IBGYNHUKJ &ll=44.8,20.46 &query=coffee &radius=1000 &openNow=1 &venuePhotos=1 &sortByDistance=1 &oauth_token=MKKKAZSMP4C3CCJUPHGERNQFSMUNRPSLTNEAUQKIYEITBBJR&v=20170930';
+var linkEx= 'https://api.foursquare.com/v2/venues/explore?client_id=XEDDHYATVRKSYW13C5HAEETLPRYAQTAPYRWWUEBZ4WZ3PRNY &client_secret=5TSQ0XOYK4HDVPJSLWC30I5DADXHHPFG54GHU30IBGYNHUKJ &ll=44.8,20.46 &query=coffee &radius=1000 &openNow=1 &venuePhotos=1 &oauth_token=MKKKAZSMP4C3CCJUPHGERNQFSMUNRPSLTNEAUQKIYEITBBJR&v=20170930';
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
@@ -14,14 +15,50 @@ var getJSON = function(url, callback) {
     xhr.send();
 };
 var result = document.getElementById('cont');
-getJSON(link,
-function(err, data) {
+var i;
+getJSON(link, function(err, data) {
   if (err !== null) {
     alert('Something went wrong: ' + err);
   } else {
-    for(var i=0;i<10;i++){
+    for(i = 0; i < 10; i++){
       var img = data.response.groups[0].items[i].venue.photos.groups[0].items[0].prefix+"300x300"+data.response.groups[0].items[i].venue.photos.groups[0].items[0].suffix;
       result.innerHTML += "<div class='list'><div class='info'><h1>" + data.response.groups[0].items[i].venue.name + "</h1><h3>" + data.response.groups[0].items[i].venue.location.distance + " m</h3></div><img src="+img+"></div>";
     }
+    var expense = document.getElementById('expense');
+    expense.onclick = function() {
+      var divs = [];
+      var removeDiv = document.getElementsByClassName('list');
+      while(removeDiv[0]){
+        removeDiv[0].parentNode.removeChild(removeDiv[0]);
+      }
+      for (i = 0; i < 10; i++) {
+        divs.push(data.response.groups[0].items[i]);
+      }
+      divs.sort(function (a,b){
+        return b.venue.price.tier - a.venue.price.tier;
+      });
+      for (i = 0; i < 10; i++) {
+        var img = divs[i].venue.photos.groups[0].items[0].prefix+"300x300"+divs[i].venue.photos.groups[0].items[0].suffix;
+        result.innerHTML += "<div class='list'><div class='info'><h1>" + divs[i].venue.name + "</h1><h3>" + divs[i].venue.location.distance + " m</h3></div><img src="+img+"></div>";
+      }
+    };
+    var destination = document.getElementById('dest');
+    destination.onclick = function() {
+      var divs = [];
+      var removeDiv = document.getElementsByClassName('list');
+      while(removeDiv[0]){
+        removeDiv[0].parentNode.removeChild(removeDiv[0]);
+      }
+      for (i = 0; i < 10; i++) {
+        divs.push(data.response.groups[0].items[i]);
+      }
+      divs.sort(function (a,b){
+        return a.venue.location.distance - b.venue.location.distance;
+      });
+      for (i = 0; i < 10; i++) {
+        var img = divs[i].venue.photos.groups[0].items[0].prefix+"300x300"+divs[i].venue.photos.groups[0].items[0].suffix;
+        result.innerHTML += "<div class='list'><div class='info'><h1>" + divs[i].venue.name + "</h1><h3>" + divs[i].venue.location.distance + " m</h3></div><img src="+img+"></div>";
+      }
+    };
   }
 });
